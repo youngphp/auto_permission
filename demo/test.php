f9e2193a5c31684af7b500f8cfaadf4f750fa66e
For you <soul_521000@live.com>
2017-11-02 13:54:41 +0800

    Update test.php

--- demo/test.php
+++ demo/test.php
@@ -1,24 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once APPPATH.'core/ERP_Controller.php';
-
 /**
-*@permission 首aaaaaab
+*@permission 首
 */
-class Test extends ERP_Controller
+class Test
 {
-	//protected $active_top_tag  = 'home';
-
+	
 
 	public function __construct(){
-		parent::__construct();
-		// $data = array();
-		// $permissions = $this->getUserPermissions();
-		// var_dump($permissions);die;
-		// $data['active_menu_tag'] = 'index';
-		// $data['task_status_types'] = $this->task_status_types;
-		// $data['active_top_tag'] = $this->active_top_tag;
-		// $this->load->vars($data);
+		
 	}
     /**
 	*@permission 首页概述
